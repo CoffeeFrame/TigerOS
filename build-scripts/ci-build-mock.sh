ae2fcbe5fce44ecdad0af0c797860047bdd80cc0
Christian Martin <25327135+ct-martin@users.noreply.github.com>
2018-01-15 20:10:11 -0500

    Move to f27 in make-iso.sh

--- build-scripts/ci-build-mock.sh
+++ build-scripts/ci-build-mock.sh
@@ -15,13 +15,13 @@ then
     exit
 fi
 #wget -O tigeros.ks https://raw.githubusercontent.com/RITlug/TigerOS/master/tigeros.ks
-mock --old-chroot -r fedora-26-x86_64 --init
-mock --old-chroot -r fedora-26-x86_64 --copyin kickstarts/tigeros.ks ./tigeros.ks
-mock --old-chroot -r fedora-26-x86_64 --install lorax-lmc-novirt git vim-minimal pykickstart
-mock --old-chroot -r fedora-26-x86_64 --install https://builder.ritlug.com/packages/x86_64/anaconda-installclass-tigeros-26-1.fc26.x86_64.rpm
-mock --old-chroot -r fedora-26-x86_64 --chroot "livemedia-creator --ks tigeros.ks --no-virt --resultdir /var/lmc --project TigerOS-Live --make-iso --volid TigerOS --iso-only --iso-name TigerOS.iso --releasever 26 --title TigerOS-live --macboot"
+mock --old-chroot -r fedora-27-x86_64 --init
+mock --old-chroot -r fedora-27-x86_64 --copyin kickstarts/tigeros.ks ./tigeros.ks
+mock --old-chroot -r fedora-27-x86_64 --install lorax-lmc-novirt git vim-minimal pykickstart
+mock --old-chroot -r fedora-27-x86_64 --install https://builder.ritlug.com/packages/x86_64/anaconda-installclass-tigeros-27-1.fc27.x86_64.rpm
+mock --old-chroot -r fedora-27-x86_64 --chroot "livemedia-creator --ks tigeros.ks --no-virt --resultdir /var/lmc --project TigerOS-Live --make-iso --volid TigerOS --iso-only --iso-name TigerOS.iso --releasever 27 --title TigerOS-live --macboot"
 rm -rf /var/www/isos/TigerOS-$(date +%Y%m%d).iso
-mock --old-chroot -r fedora-26-x86_64 --copyout /var/lmc/TigerOS.iso /var/www/isos/TigerOS-$(date +%Y%m%d).iso 
+mock --old-chroot -r fedora-27-x86_64 --copyout /var/lmc/TigerOS.iso /var/www/isos/TigerOS-$(date +%Y%m%d).iso 
 rm -rf /var/lib/mock/
 cd /var/www/isos
 rm -rf CHECKSUM512-$(date +%Y%m%d)
@@ -29,4 +29,3 @@ sha512sum TigerOS-$(date +%Y%m%d).iso > CHECKSUM512-$(date +%Y%m%d)
 chown -R nginx:nginx /var/www/
 chmod 755 /var/www/isos/*.iso
 echo "Build finished"
-
